b807e59e9157aeb04a46d2e651c1cdf73388b722
Micha Mazaheri <micha@mazaheri.me>
2015-12-19 17:29:56 +0100

    Add support for --data

--- src/CurlParser.js
+++ src/CurlParser.js
@@ -2,15 +2,22 @@ import Immutable from 'immutable'
 
 import ShellTokenizer from './ShellTokenizer'
 
+export class CurlFileReference extends Immutable.Record({
+  filepath: null
+}) { }
+
+export class CurlKeyValue extends Immutable.Record({
+  key: null,
+  value: null
+}) { }
+
 export class CurlRequest extends Immutable.Record({
   url: null,
   method: null,
   headers: Immutable.OrderedMap(),
   bodyType: null,
   body: null,
-}) {
-
-}
+}) { }
 
 const TOKEN_BREAK = Immutable.List(['|', '>', '1>', '2>', '&>'])
 
@@ -45,7 +52,7 @@ export default class CurlParser {
   _parseAll() {
     let arg
     while ((arg = this._popArg()) != null) {
-      if (arg.toLowerCase() == 'curl') {
+      if (arg.toLowerCase() === 'curl') {
         this._parseCurlCommand()
       }
     }
@@ -59,17 +66,35 @@ export default class CurlParser {
       if (TOKEN_BREAK.includes(arg)) {
         break;
       }
-      else if (arg == '-H' || arg == '--header') {
+      else if (arg === '-X' || arg === '--request') {
+        request = this._parseMethod(request)
+      }
+      else if (arg === '-I' || arg === '--head') {
+        request = request.set('method', 'HEAD')
+      }
+      else if (arg === '-H' || arg === '--header') {
         request = this._parseHeader(request)
       }
-      else if (arg == '-F' || arg == '--form') {
-        request = this._parseFormData(request)
+      else if (arg === '-F' || arg === '--form') {
+        request = this._parseMultipartFormData(request)
+      }
+      else if (arg === '-d' || arg === '--data' || arg === '--data-ascii') {
+        request = this._parseUrlEncodedData(request, '--data')
+      }
+      else if (arg === '--data-binary') {
+        request = this._parseUrlEncodedData(request, '--data-binary')
+      }
+      else if (arg === '--data-raw') {
+        request = this._parseUrlEncodedData(request, '--data-raw')
+      }
+      else if (arg === '--data-urlencode') {
+        request = this._parseUrlEncodedData(request, '--data-urlencode')
       }
       else {
         urls = urls.push(this._cleanUrl(arg))
       }
     }
-    if (request.get('method') == null) {
+    if (request.get('method') === null) {
       request = request.set('method', 'GET')
     }
     urls.forEach(url => {
@@ -85,16 +110,32 @@ export default class CurlParser {
     return url
   }
 
+  _resolveFileReference(string) {
+    const m = string.match(/^\@(.*)$/)
+    if (m) {
+      return new CurlFileReference({
+        filepath: m[1]
+      })
+    }
+    return string
+  }
+
   _parseHeader(request) {
     const arg = this._popArg()
-    const m = arg.match(/(?:^([^\:\s]+)\s*\:\s*(.*)$)/)
+    const m = arg.match(/^([^\:\s]+)\s*\:\s*(.*)$/)
     if (!m) {
       throw new Error('Invalid -H/--header value: ' + arg)
     }
     return request.setIn(['headers', m[1]], m[2])
   }
 
-  _parseFormData(request) {
+  _parseMethod(request) {
+    const arg = this._popArg()
+    return request.set('method', arg)
+  }
+
+  _parseMultipartFormData(request) {
+    // switch bodyType
     if (request.get('bodyType') != 'formData') {
       if (request.get('bodyType') != null) {
         throw new Error('Different body types set in the same request')
@@ -105,10 +146,72 @@ export default class CurlParser {
       })
     }
     const arg = this._popArg()
-    const m = arg.match(/(?:^([^\=\s]+)\s*\=\s*([^\;]*))/)
+    const m = arg.match(/^([^\=]+)\=([^\;]*)/)
     if (!m) {
       throw new Error('Invalid -F/--form value: ' + arg)
     }
-    return request.setIn(['body', m[1]], m[2])
+
+    // set body param
+    request = request.setIn(['body', m[1]], m[2])
+
+    // set method if not set
+    if (request.get('method') === null) {
+      request = request.set('method', 'POST')
+    }
+
+    return request
+  }
+
+  _parseUrlEncodedData(request, option) {
+    // switch bodyType
+    if (request.get('bodyType') != 'urlEncoded') {
+      if (request.get('bodyType') != null) {
+        throw new Error('Different body types set in the same request')
+      }
+      request = request.merge({
+        bodyType: 'urlEncoded',
+        body: Immutable.List()
+      })
+    }
+
+    const arg = this._popArg()
+    
+    if (option === '--data' || option === '--data-raw') {
+      let value = arg
+
+      // resolve file reference @filename
+      if (option === '--data') {
+        value = this._resolveFileReference(value)
+      }
+
+      // if file reference
+      if (value instanceof CurlFileReference) {
+        request = request.set('body', request.get('body').push(new CurlKeyValue({
+          key: value
+        })))
+      }
+      // otherwise, parse the parameters
+      else {
+        let components = value.split('&')
+        for (let i = 0; i < components.length; i++) {
+          const component = components[i]
+          let m = component.match(/([^\=]+)(?:\=(.*))?/)
+          request = request.set('body', request.get('body').push(new CurlKeyValue({
+            key: decodeURIComponent(m[1]),
+            value: (m[2] !== undefined ? decodeURIComponent(m[2]) : null)
+          })))
+        };
+      }
+    }
+    else {
+      throw new Error('!!! NOT IMPLEMENTED')
+    }
+
+    // set method if not set
+    if (request.get('method') === null) {
+      request = request.set('method', 'POST')
+    }
+
+    return request
   }
 }
--- src/__tests__/CurlParser-test.js
+++ src/__tests__/CurlParser-test.js
@@ -2,10 +2,15 @@ import {UnitTest, registerTest} from '../TestUtils'
 import Immutable from 'immutable'
 import fs from 'fs'
 
-import CurlParser, { CurlRequest } from '../CurlParser'
+import CurlParser, { CurlRequest, CurlKeyValue, CurlFileReference } from '../CurlParser'
 
 @registerTest
 class TestCurlParser extends UnitTest {
+
+  // 
+  // testing simple with no option
+  // 
+
   testSimple() {
     this.__testCurlRequest('curl http://httpbin.org/get', new CurlRequest({
       url: 'http://httpbin.org/get',
@@ -40,6 +45,77 @@ class TestCurlParser extends UnitTest {
     ]))
   }
 
+  // 
+  // testing -X --request options
+  // 
+
+  testMethodGET() {
+    this.__testCurlRequest('curl http://httpbin.org/get -X GET', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'GET'
+    }))
+  }
+
+  testMethodPOSTAfter() {
+    this.__testCurlRequest('curl http://httpbin.org/get -X POST', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'POST'
+    }))
+  }
+
+  testMethodPOSTBefore() {
+    this.__testCurlRequest('curl -X POST http://httpbin.org/get', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'POST'
+    }))
+  }
+
+  testMethodPOSTOverride() {
+    this.__testCurlRequest('curl http://httpbin.org/get -X PATCH -X POST', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'POST'
+    }))
+  }
+
+  testMethodPOSTLong() {
+    this.__testCurlRequest('curl http://httpbin.org/get --request POST', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'POST'
+    }))
+  }
+
+  testMethodHEAD() {
+    this.__testCurlRequest('curl http://httpbin.org/get -X HEAD', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'HEAD'
+    }))
+  }
+
+  // 
+  // testing -I --head options
+  // 
+
+  testHeadOption() {
+    this.__testCurlRequest('curl -I http://httpbin.org/get', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'HEAD'
+    }))
+  }
+
+  testHeadOptionLong() {
+    this.__testCurlRequest('curl --head http://httpbin.org/get', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'HEAD'
+    }))
+  }
+
+  testHeadOptionOverrideGET() {
+    this.__testCurlRequest('curl -I -X GET http://httpbin.org/get', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'GET'
+    }))
+  }
+
   testSimpleHeader() {
     this.__testCurlRequest('curl http://httpbin.org/get -H X-Paw:value', new CurlRequest({
       url: 'http://httpbin.org/get',
@@ -61,10 +137,36 @@ class TestCurlParser extends UnitTest {
     }))
   }
 
+  // 
+  // testing -F --form options
+  // 
+
   testSimpleFormData() {
     this.__testCurlRequest('curl http://httpbin.org/get -F key=value', new CurlRequest({
       url: 'http://httpbin.org/get',
-      method: 'GET',
+      method: 'POST',
+      bodyType: 'formData',
+      body: Immutable.OrderedMap({
+        'key': 'value'
+      })
+    }))
+  }
+
+  testSimpleFormDataMethodOverrideBefore() {
+    this.__testCurlRequest('curl http://httpbin.org/get -X PATCH -F key=value', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'PATCH',
+      bodyType: 'formData',
+      body: Immutable.OrderedMap({
+        'key': 'value'
+      })
+    }))
+  }
+
+  testSimpleFormDataMethodOverrideAfter() {
+    this.__testCurlRequest('curl http://httpbin.org/get -F key=value -X PATCH', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'PATCH',
       bodyType: 'formData',
       body: Immutable.OrderedMap({
         'key': 'value'
@@ -73,9 +175,9 @@ class TestCurlParser extends UnitTest {
   }
 
   testFormDataMultiple() {
-    this.__testCurlRequest('curl http://httpbin.org/get -F key=value -F name=Paw', new CurlRequest({
+    this.__testCurlRequest('curl http://httpbin.org/get -F key=value --form name=Paw', new CurlRequest({
       url: 'http://httpbin.org/get',
-      method: 'GET',
+      method: 'POST',
       bodyType: 'formData',
       body: Immutable.OrderedMap({
         'key': 'value',
@@ -87,13 +189,193 @@ class TestCurlParser extends UnitTest {
   testFormDataWithParams() {
     this.__testCurlRequest('curl http://httpbin.org/get -F key=value;type=text/plain', new CurlRequest({
       url: 'http://httpbin.org/get',
-      method: 'GET',
+      method: 'POST',
       bodyType: 'formData',
       body: Immutable.OrderedMap({
         'key': 'value'
       })
     }))
   }
+
+  // 
+  // testing -d --data --data-binary options
+  // 
+
+  testFormDataKeyValue() {
+    this.__testCurlRequest('curl http://httpbin.org/get -d key=value', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'POST',
+      bodyType: 'urlEncoded',
+      body: Immutable.List([
+        new CurlKeyValue({key: 'key', value: 'value'})
+      ])
+    }))
+  }
+
+  testFormDataKeyValueMultiple() {
+    this.__testCurlRequest('curl http://httpbin.org/get -d key=value --data key2=value2', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'POST',
+      bodyType: 'urlEncoded',
+      body: Immutable.List([
+        new CurlKeyValue({key: 'key', value: 'value'}),
+        new CurlKeyValue({key: 'key2', value: 'value2'})
+      ])
+    }))
+  }
+
+  testFormDataKeyValueOverrideMethodBefore() {
+    this.__testCurlRequest('curl http://httpbin.org/get -X PATCH -d key=value', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'PATCH',
+      bodyType: 'urlEncoded',
+      body: Immutable.List([
+        new CurlKeyValue({key: 'key', value: 'value'})
+      ])
+    }))
+  }
+
+  testFormDataKeyValueOverrideMethodAfter() {
+    this.__testCurlRequest('curl http://httpbin.org/get -d key=value -X PATCH', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'PATCH',
+      bodyType: 'urlEncoded',
+      body: Immutable.List([
+        new CurlKeyValue({key: 'key', value: 'value'})
+      ])
+    }))
+  }
+
+  testFormDataKeyValueUrlEncoded() {
+    this.__testCurlRequest('curl http://httpbin.org/get -d ke%20y=val%20ue', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'POST',
+      bodyType: 'urlEncoded',
+      body: Immutable.List([
+        new CurlKeyValue({key: 'ke y', value: 'val ue'})
+      ])
+    }))
+  }
+
+  testFormDataKeyValueEmptyValue() {
+    this.__testCurlRequest('curl http://httpbin.org/get -d key=', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'POST',
+      bodyType: 'urlEncoded',
+      body: Immutable.List([
+        new CurlKeyValue({key: 'key', value: ''})
+      ])
+    }))
+  }
+
+  testFormDataKeyValueNoValue() {
+    this.__testCurlRequest('curl http://httpbin.org/get -d key', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'POST',
+      bodyType: 'urlEncoded',
+      body: Immutable.List([
+        new CurlKeyValue({key: 'key', value: null})
+      ])
+    }))
+  }
+
+  testFormDataKeyValueMultipleValueInOneOption() {
+    this.__testCurlRequest('curl http://httpbin.org/get -d "key=value&key2=value2"', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'POST',
+      bodyType: 'urlEncoded',
+      body: Immutable.List([
+        new CurlKeyValue({key: 'key', value: 'value'}),
+        new CurlKeyValue({key: 'key2', value: 'value2'})
+      ])
+    }))
+  }
+
+  testFormDataKeyValueMultipleValueInOneOptionUrlEncoded() {
+    this.__testCurlRequest('curl http://httpbin.org/get -d "ke%20y=value&key2=value%2F2"', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'POST',
+      bodyType: 'urlEncoded',
+      body: Immutable.List([
+        new CurlKeyValue({key: 'ke y', value: 'value'}),
+        new CurlKeyValue({key: 'key2', value: 'value/2'})
+      ])
+    }))
+  }
+
+  testFormDataKeyValuePlayingWithEscapes() {
+    this.__testCurlRequest('curl http://httpbin.org/get -d $\'key=v\\x61lue\'', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'POST',
+      bodyType: 'urlEncoded',
+      body: Immutable.List([
+        new CurlKeyValue({key: 'key', value: 'value'})
+      ])
+    }))
+  }
+
+  testFormDataKeyValueFileReference() {
+    this.__testCurlRequest('curl http://httpbin.org/get -d @filename.txt', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'POST',
+      bodyType: 'urlEncoded',
+      body: Immutable.List([
+        new CurlKeyValue({key: new CurlFileReference({filepath: 'filename.txt'}), value: null})
+      ])
+    }))
+  }
+
+  testFormDataKeyValueFileReferenceDataAscii() {
+    this.__testCurlRequest('curl http://httpbin.org/get --data-ascii @filename.txt', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'POST',
+      bodyType: 'urlEncoded',
+      body: Immutable.List([
+        new CurlKeyValue({key: new CurlFileReference({filepath: 'filename.txt'}), value: null})
+      ])
+    }))
+  }
+
+  testFormDataKeyValueNoFileReferenceInRaw() {
+    this.__testCurlRequest('curl http://httpbin.org/get --data-raw @filename.txt', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'POST',
+      bodyType: 'urlEncoded',
+      body: Immutable.List([
+        new CurlKeyValue({key: '@filename.txt', value: null})
+      ])
+    }))
+  }
+
+  testFormDataKeyValueFileReferenceMultiple() {
+    this.__testCurlRequest('curl http://httpbin.org/get -d @filename.txt --data @filename2.txt', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'POST',
+      bodyType: 'urlEncoded',
+      body: Immutable.List([
+        new CurlKeyValue({key: new CurlFileReference({filepath: 'filename.txt'}), value: null}),
+        new CurlKeyValue({key: new CurlFileReference({filepath: 'filename2.txt'}), value: null})
+      ])
+    }))
+  }
+
+  testFormDataKeyValueFileReferenceAndParams() {
+    this.__testCurlRequest('curl http://httpbin.org/get -d @filename.txt -d @filename2.txt --data "name=Paw&key2=value2"', new CurlRequest({
+      url: 'http://httpbin.org/get',
+      method: 'POST',
+      bodyType: 'urlEncoded',
+      body: Immutable.List([
+        new CurlKeyValue({key: new CurlFileReference({filepath: 'filename.txt'}), value: null}),
+        new CurlKeyValue({key: new CurlFileReference({filepath: 'filename2.txt'}), value: null}),
+        new CurlKeyValue({key: 'name', value: 'Paw'}),
+        new CurlKeyValue({key: 'key2', value: 'value2'}),
+      ])
+    }))
+  }
+
+  // 
+  // helpers
+  // 
 
   __testCurlRequest(input, expected) {
     this.__testCurlRequests(input, Immutable.List([expected]))
