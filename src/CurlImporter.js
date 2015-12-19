d4bf72b2479c91d686bb6418fceaf7696e2b9254
Micha Mazaheri <micha@mazaheri.me>
2015-12-20 00:03:37 +0100

    Fix Paw import of escaped sequences and files

--- src/CurlImporter.js
+++ src/CurlImporter.js
@@ -13,6 +13,57 @@ class CurlImporter {
     return value
   }
 
+  _escapeSequenceDynamicValue(seq) {
+    let escapeSequence = ''
+    for (let i = 0; i < seq.length; i++) {
+      const char = seq[i]
+      if (char === "\n") { escapeSequence += "\\n" }
+      else if (char === "\r") { escapeSequence += "\\r" }
+      else if (char === "\t") { escapeSequence += "\\t" }
+      else {
+        let hexChar = char.charCodeAt(0).toString(16)
+        if (hexChar.length === 1) {
+          hexChar = '0' + hexChar
+        }
+        escapeSequence += "\\x" + hexChar
+      }
+    };
+    return new DynamicValue("com.luckymarmot.EscapeSequenceDynamicValue", {
+      escapeSequence: escapeSequence
+    })
+  }
+
+  _toDynamicString(string, defaultToEmpty, resolveFileRefs) {
+    if (!string) {
+      if (defaultToEmpty) {
+        return new DynamicString('')
+      }
+      return null
+    }
+
+    // resolve file references
+    if (resolveFileRefs) {
+      const resolvedString = this._resolveFileReference(string)
+      if (resolvedString instanceof DynamicString) {
+        return resolvedString
+      }
+    }
+
+    // split around special characters
+    const re = /([^\x00-\x1f]+)|([\x00-\x1f]+)/gm
+    let components = []
+    let m
+    while ((m = re.exec(string)) !== null) {
+      if (m[1]) {
+        components.push(m[1])
+      }
+      else {
+        components.push(this._escapeSequenceDynamicValue(m[2]))
+      }
+    }
+    return new DynamicString(...components)
+  }
+
   _importPawRequest(context, curlRequest) {
     const headers = curlRequest.get('headers')
     const auth = curlRequest.get('auth')
@@ -20,11 +71,18 @@ class CurlImporter {
     const body = curlRequest.get('body')
 
     // url + method
-    let pawRequest = context.createRequest("cURL", curlRequest.get('method'), curlRequest.get('url'))
+    let pawRequest = context.createRequest(
+      "cURL",
+      curlRequest.get('method'),
+      this._toDynamicString(curlRequest.get('url'), true, true),
+    )
 
     // headers
     headers.forEach((value, key) => {
-      pawRequest.setHeader(key, value)
+      pawRequest.setHeader(
+        this._toDynamicString(key, true, true),
+        this._toDynamicString(value, true, true)
+      )
     })
 
     // auth
@@ -48,18 +106,12 @@ class CurlImporter {
     else if (bodyType === 'urlEncoded') {
       // this is not form url encoded, but a plain body string or file
       if (body.count() === 1 && !body.getIn([0, 'value'])) {
-        pawRequest.body = this._resolveFileReference(body.getIn([0, 'key'], ''))
+        pawRequest.body = this._toDynamicString(body.getIn([0, 'key']), true, true)
       }
       else {
         const keyValues = body.map(keyValue => {
-          let key = this._resolveFileReference(keyValue.get('key'))
-          let value = this._resolveFileReference(keyValue.get('value'))
-          if (!(key instanceof DynamicString)) {
-            key = new DynamicString(key || '')
-          }
-          if (!(value instanceof DynamicString)) {
-            value = new DynamicString(value || '')
-          }
+          let key = this._toDynamicString(keyValue.get('key'), true, true)
+          let value = this._toDynamicString(keyValue.get('value'), true, true)
           return [key, value, true]
         }).toArray()
         const dv = new DynamicValue("com.luckymarmot.BodyFormKeyValueDynamicValue", {
@@ -79,6 +131,9 @@ class CurlImporter {
   importString(context, string) {
     const parser = new CurlParser()
     const curlRequests = parser.parse(string)
+    if (curlRequests.count() === 0) {
+      throw new Error('No curl request found')
+    }
     this._importPawRequests(context, curlRequests)
     return true
   }
